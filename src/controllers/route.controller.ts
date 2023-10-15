9444599511b1b93cdc2bd12ad901afad6e4389ec
Jose Garcia <luizg9414@gmail.com>
2023-10-15 05:40:44 -0600

    :sparkles: Add validations

--- src/controllers/route.controller.ts
+++ src/controllers/route.controller.ts
@@ -5,31 +5,49 @@ import { IPoint, IRoute } from "../interfaces";
 import { maps } from "../helpers";
 
 export const createRoute = async (req:Request , res:Response) => {
-    const { from="", to="" } = req.body
+    const { from, to } = req.body
 
-    await db.connect()
-    const [ From ]:IPoint[] = await Point.find({ "location.name" : from })
-    const [ To ]:IPoint[] = await Point.find({ "location.name" : to })
-    await db.disconnect()
+    try {
+        await db.connect()
+        const From:IPoint|null = await Point.findOne({ "location.name" : from })
+        const To:IPoint|null = await Point.findOne({ "location.name" : to })
+        const RepiteRoute:IRoute|null = await Route.findOne({ from, to }).exec()
+        await db.disconnect()
+
+        // Valida que existan los dos puntos
+        if (!(From && To)){
+            throw "Parametros invalido en el body"
+        }
+
+        // Valida si ya existe una ruta
+        if (RepiteRoute){
+            throw "Ya existe una ruta"
+        }
+
+        const idFrom = From.location.placeId
+        const locationFrom = await maps.getCoordinates(idFrom)
+        
+        const idTo = To.location.placeId
+        const locationTo = await maps.getCoordinates(idTo)
     
-    const idFrom = From.location.placeId
-    const locationFrom = await maps.getCoordinates(idFrom)
+        const kmInRoute = await maps.getKm(idFrom, idTo)
     
-    const idTo = To.location.placeId
-    const locationTo = await maps.getCoordinates(idTo)
-
-    const kmInRoute = await maps.getKm(idFrom, idTo)
-
-
-    return res.json({
-        idFrom,
-        locationFrom,
-
-        idTo,
-        locationTo,
-
-        kmInRoute
-    })
+    
+        return res.json({
+            idFrom,
+            locationFrom,
+    
+            idTo,
+            locationTo,
+    
+            kmInRoute
+        }) 
+        
+    } catch (error) {
+        return res.status(406).json({
+            "mesage": error
+        })
+    }
 }
 
 export const getAll = async (req:Request , res:Response) => {
