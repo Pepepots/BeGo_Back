7697db3a9092aee88d9e8050e385c6217dadbce0
Jose Garcia <luizg9414@gmail.com>
2023-10-15 00:24:36 -0600

    :sparkles: route point by id

--- src/routes/points.routes.ts
+++ src/routes/points.routes.ts
@@ -4,5 +4,6 @@ import { pointController} from '../controllers'
 const router = Router();
 
 router.get('/points', pointController.getAll)
+router.get('/points/:id', pointController.getById)
 
 export default router
